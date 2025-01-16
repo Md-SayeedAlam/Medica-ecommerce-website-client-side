e118cb3eb76741502f36e4be9f13777c960bf64c
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2025-01-16 14:34:17 +0600

    added jwt authentication on axios Secure page and auth provider

--- src/Hookos/AuthProvider/AuthProvider.jsx
+++ src/Hookos/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,12 @@
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
 import React, { Children, createContext, useEffect, useState } from 'react';
 import { auth } from '../../Firebase/firebase.config';
+import useAxiosPublic from './../useAxiosPublic';
 
 export const AuthContext = createContext(null)
 
 const AuthProvider = ({children}) => {
-
+    const axiosPublic = useAxiosPublic()
     const [user,setUser] = useState(null)
     const [loading,setLoading] = useState(true)
     const googleProvider = new GoogleAuthProvider()
@@ -57,13 +58,30 @@ useEffect(()=>{
     const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
         setUser(currentUser)
         console.log('currently logged in',currentUser)
-        setLoading(false)
+        if(currentUser){
+            // get Token and store
+            const userInfo = {email:currentUser.email}
+            axiosPublic.post('/jwt',userInfo)
+            .then(res=>{
+                if(res.data.token){
+                    localStorage.setItem('ACCESS TOKEN',res.data.token)
+                    setLoading(false)
+                }
+            })
+        }else{
+            // TODO remove token
+
+            localStorage.removeItem('ACCESS TOKEN')
+            setLoading(false)
+        }
+        
+    
     })
     
     return()=>{
         unsubscribe()
     }
-},[])
+},[axiosPublic])
 
 
 
--- src/Hookos/useAxiosSecure.jsx
+++ src/Hookos/useAxiosSecure.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import UseAuth from './UseAuth';
 
 
 
@@ -8,6 +10,36 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+
+    const navigate = useNavigate()
+    const{logOut}=UseAuth()
+
+    axiosSecure.interceptors.request.use(function(config){
+        const token = localStorage.getItem('ACCESS TOKEN')
+        // console.log('request stopped by interceptors',token)
+        config.headers.authorization= `Bearer ${token}`
+        return config;
+    },
+    function(error){
+        return Promise.reject(error)
+    }
+)
+
+// intercepts 401 and 403 status
+axiosSecure.interceptors.response.use(function(response){
+    return response
+},async(error)=>{
+    const status = error.response.status
+    console.log('status error',status)
+    // for 401 or 403 logout the user and move the user to the login page
+    if(status === 401 ||403 ){
+        await logOut()
+        navigate('/login')
+    }
+    return Promise.reject(error)
+})
+
+
 
    return axiosSecure
 };
